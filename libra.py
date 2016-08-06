2bc9ef66f6e842e426558aaca2a8d997924ce7db
DaEunKim <ekdms717@naver.com>
2016-08-06 23:45:55 +0900

    librarysample.py

---
+++ libra.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+canvas_height=400
+canvas_width=600
+canvas_color="black"
+
+p1_x = canvas_width/2
+p1_y = canvas_height
+p1_color="green"
+line_width=5
+line_length=5
+
+def p1_move_N(event):
+    pass
+def p1_move_S(event):
+    pass
+
+def p1_move_E(event):
+    pass
+
+def p1_move_W(event):
+    pass
+
+def erase_all(event):
+    pass
