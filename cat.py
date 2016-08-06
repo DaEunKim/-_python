0774bebf698854d98660e77f8aabcd6925b9ef7c
DaEunKim <ekdms717@naver.com>
2016-08-06 23:46:45 +0900

    cat.py

---
+++ cat.py
@@ -0,0 +1,18 @@
+class Cat:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(self.name, "mew")
+    
+    def drink(self):
+        print(self.name, "drink milk")
+        print(self.name, "napping")
+
+    def fight(juliet):
+        print(self.name,"ㅇㅔㄱㅔ jeliet ㅇㅣ ㅆㅏㅇㅜㅁㅡㅇㄹ ㄱㅓㄹㅇㅓㅇㅗㅏㅅㄷㅏ")
+        if(len(self)>len(self.name)):
+            print(self,"win")
+        else:
+            print(self.name,"win")
+
